6cd0fd61a9cc2900df6e0d8aebe2bd9b63760fa1
Harideep KS <harideepkshyam2@gmail.com>
2023-05-05 16:48:40 +0530

    fdsad

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -39,7 +39,7 @@ public class UserService {
         Integer cnt=0;
 
         for (WebSeries webSeries:webSeriesList){
-            if(webSeries.getAgeLimit()<=user.getAge() && webSeries.getSubscriptionType().toString().equals(user.getSubscription().getSubscriptionType().toString())){
+            if(webSeries.getAgeLimit()<=user.getAge() && webSeries.getSubscriptionType()==user.getSubscription().getSubscriptionType()){
                 cnt++;
             }
         }
