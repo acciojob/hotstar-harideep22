27d0c25bbdcc74afc7940d3651d818760f6dc219
Harideep KS <harideepkshyam2@gmail.com>
2023-05-05 16:14:25 +0530

    4th try

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -7,6 +7,8 @@ import com.driver.repository.ProductionHouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class ProductionHouseService {
 
@@ -14,7 +16,9 @@ public class ProductionHouseService {
     ProductionHouseRepository productionHouseRepository;
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
-        ProductionHouse productionHouse=new ProductionHouse(productionHouseEntryDto.getName());
+        ProductionHouse productionHouse=new ProductionHouse();
+        productionHouse.setName(productionHouseEntryDto.getName());
+        productionHouse.setWebSeriesList(new ArrayList<>());
         productionHouse.setRatings(0);
 
         productionHouseRepository.save(productionHouse);
